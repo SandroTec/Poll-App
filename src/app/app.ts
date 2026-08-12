f94aa52739b1ae686c023ac0df4a4a4079060afe
Sandro <Sandro.bonomo@gmx.de>
2026-08-12 19:10:44 +0200

    feat: add main component and import in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
+import { Main } from './shared/components/main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
