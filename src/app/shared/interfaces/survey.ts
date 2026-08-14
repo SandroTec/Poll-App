b8544a59c9d14c9441a23001e240119a06e02472
Sandro <Sandro.bonomo@gmx.de>
2026-08-14 12:41:53 +0200

    feat: basic survey structure in survey interface

--- src/app/shared/interfaces/survey.ts
+++ src/app/shared/interfaces/survey.ts
@@ -1,1 +1,10 @@
-export interface Survey {}
+export interface Survey {
+
+    id: number;
+    title:string;
+    description: string;
+    created_at: Date;
+    ends_at: Date;
+    category: string;
+    
+}
