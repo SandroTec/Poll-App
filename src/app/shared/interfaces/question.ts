4d6336f9154b0b68e2b27dfde55a867353f3d6ad
Sandro <Sandro.bonomo@gmx.de>
2026-08-14 17:11:13 +0200

    feat: interfaces for questions and answers. set them in survey service

---
+++ src/app/shared/interfaces/question.ts
@@ -0,0 +1,6 @@
+export interface Question {
+    id: number;
+    survey_id: number;
+    title: string;
+    
+}
--- src/app/shared/services/survey.service.ts
+++ src/app/shared/services/survey.service.ts
@@ -1,6 +1,9 @@
 import { Injectable, signal } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
 import { Survey } from '../interfaces/survey';
+import { Question } from '../interfaces/question';
+import { Answer } from '../interfaces/answer';
+
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +12,9 @@ export class SurveyService {
 
   supabase = createClient('https://zlbbjaxdutacpsrcqhrz.supabase.co/','sb_publishable_3yQFBsKbiKiTv7vtWH2hZQ_CJkhtaid');
 
-  surveyList = signal<Survey[]>([])
+  surveyList = signal<Survey[]>([]);
+  questionList = signal<Question[]>([]);
+  answerList = signal<Answer[]>([]);
 
   constructor() {
     this.getSurveys();
@@ -23,11 +28,15 @@ export class SurveyService {
   }
 
   async getQuestions(surveyId: number) {
-    return this.supabase.from('survey_questions').select('*').eq('survey_id', surveyId);
+    let response = await this.supabase.from('survey_questions').select('*').eq('survey_id', surveyId);
+    this.questionList.set((response.data ?? []) as Question[]);
+
   }
 
   async getAnswers(questionId: number) {
-    return this.supabase.from('question_answers').select('*').eq('question_id', questionId);
+    let response = await this.supabase.from('question_answers').select('*').eq('question_id', questionId);
+    this.answerList.set((response.data ?? []) as Answer[]);
+
   }
   
 }
