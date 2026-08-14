fbc734de5501a87bc08b72ac3f6b2fca2a25234d
Sandro <Sandro.bonomo@gmx.de>
2026-08-14 12:45:40 +0200

    feat: implement surveys component in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Main } from './shared/components/main/main';
+import { Surveys } from './shared/components/surveys/surveys';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Main],
+  imports: [RouterOutlet, Header, Main, Surveys],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
