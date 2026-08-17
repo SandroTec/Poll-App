35ae2ecb63efce5fba1cc642d7080d495a2b6f54
Sandro <Sandro.bonomo@gmx.de>
2026-08-17 13:21:51 +0200

    feat: add form groups, arrays and controls for survey dialog style: start dialog styling fix: connect form groups, arrays and controls in HTML

--- src/app/shared/components/add-survey-modal/add-survey-modal.ts
+++ src/app/shared/components/add-survey-modal/add-survey-modal.ts
@@ -1,8 +1,9 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-add-survey-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-survey-modal.html',
   styleUrl: './add-survey-modal.scss',
 })
@@ -11,9 +12,42 @@ export class AddSurveyModal {
   //safes dialog html element as dialog via ViewChild
   @ViewChild('dialog') dialog!: ElementRef<HTMLDialogElement>;
 
-
   // classic showModal to display dialog when button in main is clicked
   openModal() {
     this.dialog.nativeElement.showModal()
+  }
+
+  surveyForm = new FormGroup({
+    title: new FormControl('n/a', {nonNullable: true, validators: [Validators.required]}),
+    description: new FormControl('n/a', {nonNullable: true, validators: [Validators.required]}),
+    category: new FormControl('n/a', {nonNullable: true, validators: [Validators.required]}),
+    ends_at: new FormControl(''),
+    questions: new FormArray([this.createQuestionForm()]),
+  })
+
+  createQuestionForm() {
+    return new FormGroup({
+      question: new FormControl('n/a', {nonNullable: true, validators: [Validators.required]}),
+      multiple_answers: new FormControl(true, {nonNullable: true}),
+      answers: new FormArray([this.createAnswersForm()]),
+    });
+  }
+
+  createAnswersForm() {
+    return new FormGroup({
+      answer: new FormControl('n/a', {nonNullable: true, validators: [Validators.required]}),
+    });
+  }
+
+  addQuestion() {
+    const questions = this.surveyForm.get('questions') as FormArray;
+    questions.push(this.createQuestionForm());
+  }
+
+  addAnswers(questionIndex: number) {
+    const questions = this.surveyForm.get('questions') as FormArray;
+    const question = questions.at(questionIndex) as FormGroup;
+    const answers = question.get('answers') as FormArray;
+    answers.push(this.createAnswersForm());
   }
 }
--- src/app/shared/interfaces/survey.ts
+++ src/app/shared/interfaces/survey.ts
@@ -3,8 +3,7 @@ export interface Survey {
     id: number;
     title:string;
     description: string;
-    created_at: Date;
-    ends_at: Date;
     category: string;
-    
+    created_at: Date;
+    ends_at?: Date;
 }
