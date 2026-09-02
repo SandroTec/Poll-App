9a1daa13e1c20c6ad2cca9dc93e8dae6d3816101
Sandro <Sandro.bonomo@gmx.de>
2026-09-02 10:15:16 +0200

    refactor: deleting unused components

--- src/app/shared/components/edit-survey/edit-survey.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-edit-survey',
-  imports: [],
-  templateUrl: './edit-survey.html',
-  styleUrl: './edit-survey.scss',
-})
-export class EditSurvey {}
--- src/app/shared/components/survey-form/survey-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-survey-form',
-  imports: [],
-  templateUrl: './survey-form.html',
-  styleUrl: './survey-form.scss',
-})
-export class SurveyForm {}
