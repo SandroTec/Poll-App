283863d488473a88f9610c2aa324763fccb530ed
Sandro <Sandro.bonomo@gmx.de>
2026-09-17 11:17:04 +0200

    refactor: deleting not needed error handeling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, ErrorHandler } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
-export class CustomErrorHandler implements ErrorHandler {
-  handleError(error: any): void {
-    const message = error?.message || error?.toString() || '';
-        if (message.includes("reading 'startTime'")) {
-      return;
-    }
-
-    console.error(error);
-  }
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
 };
